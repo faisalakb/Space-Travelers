9b13fe5f925f7313b9528fe210b643fd2ad29003
faisalakb <faisalakb786@gmail.com>
2023-09-05 20:56:52 +0500

    Add mission action,reducer and store

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import DisplayContent from './component/DisplayContent';
 import './App.css';
+import './component/Missions/Mission.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider store={store}>
+        <DisplayContent />
+      </Provider>
+    </>
   );
 }
 
