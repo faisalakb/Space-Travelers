db4b3aba9f174372475cd2f4ef38aab87c1d3348
Bett Enock <bettenock968@gmail.com>
2023-09-05 14:43:37 -0700

    Remove comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './component/NavBar';
 import Mission from './component/Missions/Mission';
