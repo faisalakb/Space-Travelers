8d77400751f1c2e472201659fb47cd1213194896
Bett Enock <bettenock968@gmail.com>
2023-09-05 14:37:42 -0700

    wrap root component with routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './component/NavBar';
+import Mission from './component/Missions/Mission';
+import Rocket from './component/Rockets/Rocket';
+import MyProfile from './component/MyProfile/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Rocket />} />
+          <Route path="Mission" element={<Mission />} />
+          <Route path="MyProfile" element={<MyProfile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
