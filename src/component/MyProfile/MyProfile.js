82ad200d0efc681175ce0d4bcb2a24b174f82a11
faisalakb <faisalakb786@gmail.com>
2023-09-06 20:21:53 +0500

    add rocket my-profile

--- src/component/MyProfile/MyProfile.js
+++ src/component/MyProfile/MyProfile.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './MyProfile.module.css';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const rocketState = useSelector((state) => state.rockets);
+  console.log(rocketState);
+
+  return (
+    <>
+      {rocketState.isLoading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div>
+          <h1>My Mission</h1>
+          <ul>
+            {rocketState.rockets.map((current) => (
+              current.reserved ? (
+                <li className="myProfile-items" key={current.id}>{current.name}</li>
+              ) : null
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default MyProfile;
