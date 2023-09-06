82c13b213f8dee2e977319db20bd560a658432b9
faisalakb <faisalakb786@gmail.com>
2023-09-06 17:22:38 +0500

    Add my-profile logic

--- src/component/MyProfile/MyProfile.js
+++ src/component/MyProfile/MyProfile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './MyProfile.module.css';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const missionState = useSelector((state) => state.missionSlice);
+
+  return (
+    <>
+      {missionState.isLoading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div>
+          <h1>My Mission</h1>
+          <ul>
+            {missionState.data.map((current) => (
+              current.reserved ? (
+                <li className="myProfile-items" key={current.mission_id}>{current.mission_name}</li>
+              ) : null
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default MyProfile;
