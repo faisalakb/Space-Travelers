4c736e967cbcf859b4ce0fec9cbc25375856bfa7
faisalakb <faisalakb786@gmail.com>
2023-09-08 19:22:11 +0500

    Updated linters disable

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { NavLink } from 'react-router-dom';
 import planet from './assets/planet.png';
 import './NavBar.css';
